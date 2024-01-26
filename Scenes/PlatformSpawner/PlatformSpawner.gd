1c1dcd6e5aa3e86f0696a3e1ff35ca9c1c6eb8f8
Niku Grönberg <gronbergniku@gmail.com>
2024-01-26 22:52:06 +0200

    add platform changes on y

--- Scenes/PlatformSpawner/PlatformSpawner.gd
+++ Scenes/PlatformSpawner/PlatformSpawner.gd
@@ -6,13 +6,15 @@ extends Node
 
 var platform
 var t = 0.0
+var collision_shape
 
 
 func _ready():
 	print("Map generator ready")
-	platform = preload("res://Scenes/Platform.tscn")
+	platform = preload("res://Scenes/Platform/Platform.tscn")
 	if platform == null:
 		print("No platform :((")
+	collision_shape = $CollisionShape3D
 
 
 func _process(delta):
@@ -25,7 +27,11 @@ func _process(delta):
 
 
 func _create_platform():
+	var size = collision_shape.get_shape().size
+	var y_position = randf_range(-size.y / 2.0, size.y / 2.0)
+
 	var instance = platform.instantiate()
 	instance.position = Vector3.ZERO
+	instance.position.y = y_position
 	instance.velocity = Vector3.LEFT * platform_speed
 	add_child(instance)
