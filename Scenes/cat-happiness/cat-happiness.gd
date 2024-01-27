fee5047e67ac156bbc17666b07e4bbd4d172830d
Niku Grönberg <gronbergniku@gmail.com>
2024-01-27 18:32:28 +0200

    cat happiness changes animations

--- Scenes/cat-happiness/cat-happiness.gd
+++ Scenes/cat-happiness/cat-happiness.gd
@@ -3,29 +3,26 @@ extends Control
 var happyScore = 0
 var player_id = 0
 
+
 func _ready():
-	
-	while(true):
-		if (happyScore >= 100):
+	while true:
+		if happyScore >= 100:
 			$"/root/Gamestate".increment_happiness(player_id, -100)
 		await get_tree().create_timer(0.5).timeout
 		$"/root/Gamestate".increment_happiness(player_id)
-	
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
+	var mood = $"/root/Gamestate".get_mood(player_id)
 	happyScore = $"/root/Gamestate".get_happiness_score(player_id)
-	
-	if (happyScore <= 10):
-		$"Cat-pic".changeCat("grumpy")
-	elif (10 < happyScore && happyScore <= 50):
-		$"Cat-pic".changeCat("cool")
-	elif (50 < happyScore && happyScore <= 100):
-		$"Cat-pic".changeCat("manic")
-		
+
+	match mood:
+		"angry":
+			$"Cat-pic".changeCat("grumpy")
+		"neutral":
+			$"Cat-pic".changeCat("cool")
+		"happy":
+			$"Cat-pic".changeCat("manic")
+
 	$"HappinessBar".value = happyScore
-	
-	
-	
-	
-	
