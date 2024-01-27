a4358b1e4eef4725e3f63701819ea954e69e5e28
petterigit <p-makela@outlook.com>
2024-01-27 20:26:56 +0200

    cat-happy-pulse

--- Scenes/cat-happiness/Cat-pic.gd
+++ Scenes/cat-happiness/Cat-pic.gd
@@ -5,12 +5,19 @@ var coolCat = load("res://assets/cat_faces/NEUTRAL.png")
 var manicCat= load("res://assets/cat_faces/HAPPY.png")
 
 
+
 # Cat type = "grumpy" | "cool" | "manic"
 func changeCat(catType: String):
+	var animationPlayer: AnimationPlayer = $AnimationPlayer
 	match catType:
 		"grumpy":
+			animationPlayer.stop()
 			set_texture(grumpyCat)
 		"cool":
+			animationPlayer.stop()
 			set_texture(coolCat)
 		"manic":
+			animationPlayer.play("pulse")
 			set_texture(manicCat)
+			
+			
