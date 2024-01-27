26211253b6ffe40d722ee5a2f0a53e7e20e1a039
Apezkin <51073470+Apezkin@users.noreply.github.com>
2024-01-27 14:06:38 +0200

    kodic plaer id

--- main.gd
+++ main.gd
@@ -6,7 +6,7 @@ var player
 func _ready():
 	player = preload("res://Scenes/player/player.tscn")
 	for n in $"/root/Gamestate".get_playercount():
-		_spawn_player(Vector3.ZERO)
+		_spawn_player(Vector3.ZERO, $"/root/Gamestate".get_all_players().keys()[n])
 	
 
 
@@ -14,8 +14,9 @@ func _ready():
 func _process(delta):
 	pass
 
-func _spawn_player(pos: Vector3):
+func _spawn_player(pos: Vector3, id):
 	var instance = player.instantiate()
 	instance.position = pos
 	instance.position.z += 1
+	instance.player_id = id
 	add_child(instance)
