8f49b2a62c8c0669a2e0bf76a9d09abca4ef84d2
Apezkin <51073470+Apezkin@users.noreply.github.com>
2024-01-27 12:00:50 +0200

    give player another dimension to think about

--- Scenes/player/player.gd
+++ Scenes/player/player.gd
@@ -1,37 +1,41 @@
-extends Area2D
+extends CharacterBody3D
 
-@export var speed = 400
-var screen_size
+@export var speed = 2
 @onready var _animated_sprite = $AnimatedSprite2D
+var target_velocity = Vector3.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	screen_size = get_viewport_rect().size
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
+func _physics_process(delta):
+	var direction = Vector3.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
+		direction.x += 1
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
+		direction.x -= 1
 	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
+		direction.y -= 1
 	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+		direction.y += 1
 	if Input.is_action_just_released("slap"):
-		slap()
+		_slap()
 
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
 		_animated_sprite.play("walk")
 	else:
 		if _animated_sprite.animation != "slap":
 			_animated_sprite.play("idle")
 		
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
+	target_velocity.x = direction.x * speed
+	target_velocity.y = direction.y * speed
 	
-func slap():
+	velocity = target_velocity
+	move_and_slide()
+	
+	
+func _slap():
 	_animated_sprite.play("slap")
